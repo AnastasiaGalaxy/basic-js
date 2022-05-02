df65b37aa28e8b9bd72149e4b037bc095da11ca3
Anastasia Galazhu <anastasiya_galaxy@mail.ru>
2022-05-03 00:35:12 +0300

    complete task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function createDreamTeam(members) {
+//   arrOfNames = [];
+//   for (let i = 0; i < members.length; i++) {
+//     if (typeof members[i] == 'string') {
+//       arrOfNames.push(members[i].split(' ').join(''))
+  
+//       console.log(arrOfNames);
+//       //arrOfNames.push(members[i].charAt(0));
+//     } 
+//   } 
+//   console.log(arrOfNames.sort().join(''));
+//   return arrOfNames.sort().join('');
+// }
+
+function createDreamTeam(members) {
+  if (members == null) {
+    return false;
+  }
+  arrOfNames = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == 'string') {
+      arrOfNames.push(members[i].split(' ').join('').toUpperCase().charAt(0))
+    } 
+  } 
+  return arrOfNames.sort().join('');
 }
 
 module.exports = {
